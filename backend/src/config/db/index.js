874a41c1fd7a3d8eb07513e332d6526dd49b7f8e
unknown <duclnph24870@fpt.edu.vn>
2023-01-07 01:32:44 +0700

    fix: change database online

--- backend/src/config/db/index.js
+++ backend/src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect () {
     try {
         mongoose.set('strictQuery',false);
-        await mongoose.connect('mongodb://127.0.0.1:27017/zingmp3ph24870');
+        await mongoose.connect('mongodb+srv://duclnph24870:ngocduc13102001@zingmp3clone.mzfoxwc.mongodb.net/zingmp3ph24870');
         console.log('Server connect success!');
     } catch (error) {
         console.log(error);
