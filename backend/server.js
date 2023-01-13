fa60db0564fac791bf5eaed77e0f9a988ecb3ad9
unknown <duclnph24870@fpt.edu.vn>
2023-01-13 21:30:50 +0700

    fix: change server.js fil

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,27 @@
-const app = require('./src');
 require('dotenv').config();
 const port = process.env.PORT || 3131;
-app.use('/', (req,res) => {
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+const routes = require('./src/routes');
+const db = require('./src/config/db');
+const path  = require('path');
+
+db.connect();
+app.use(express.static(path.join(__dirname,'src/asset')));
+
+app.use(cors({ origin: true }),express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+
+routes(app);
+
+app.get('/', (req,res) => {
     return res.send('server on');
 });
+
+
 app.listen(port, () => {
     console.log('http://localhost:'+port);
 });
